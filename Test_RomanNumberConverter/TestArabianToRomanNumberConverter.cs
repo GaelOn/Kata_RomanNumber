8824d3d18168d4d8bdcb46e11a3942d7b70fed47
MoonRabbit <dupiregael@yahoo.fr>
2017-02-10 21:08:59 +0100

    First commit red test

---
+++ Test_RomanNumberConverter/TestArabianToRomanNumberConverter.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections;
+
+namespace Test_RomanNumberConverter
+{
+    [TestFixture]
+    public class Test
+    {
+        ArabianToRomanNumberConverter _converter;
+
+        [SetUp]
+        void Init()
+        {
+            var _converter = new ArabianToRomanNumberConverter();
+        }
+
+        [Test, TestCaseSource(typeof(GivenData), "TestBasicCases")]
+        public void TestCase(int toBeConverted, string intendedValue)
+        {
+            var convertionResult = _converter.Convert(toBeConverted);
+            convertionResult.ShouldBeEquivalent(intendedValue, $"because when i try to convert {toBeconverted} then i should get {intendedValue} as a result not {convertionResult}.");
+        }
+    }
+
+    public static class GivenData
+    {
+        public static IEnumerable TestBasicCases
+        {
+            get
+            {
+                yield return new TestCaseData(1, 'I');
+            }
+        }
+    }
+}
