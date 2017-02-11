9493820efd007cbfbc778786b5a177012538c4a2
MoonRabbit <dupiregael@yahoo.fr>
2017-02-11 22:51:27 +0100

    minimal code to pass the test

--- Kata_RomanNumber_TDD/ArabianToRomanNumberConverter.cs
+++ Kata_RomanNumber_TDD/ArabianToRomanNumberConverter.cs
@@ -12,6 +12,7 @@ namespace Kata_RomanNumber_TDD
         static RomanUnit()
         {
             Member = new Dictionary<int, string>();
+            Member.Add(400, "CD");
             Member.Add(100, "C");
             Member.Add(90, "XC");
             Member.Add(50, "L");
