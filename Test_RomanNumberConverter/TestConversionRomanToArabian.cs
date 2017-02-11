eeb9a9de3193a390493d834d6527b90fe66c9b74
MoonRabbit <dupiregael@yahoo.fr>
2017-02-12 00:45:28 +0100

    Add test to convert Roman to arabian

--- Test_RomanNumberConverter/TestConversionRomanToArabian.cs
+++ Test_RomanNumberConverter/TestConversionRomanToArabian.cs
@@ -1,22 +1,23 @@
-﻿using NUnit.Framework;
+using NUnit.Framework;
 using FluentAssertions;
 using Kata_RomanNumber_TDD;
 
 namespace Test_RomanNumberConverter
 {
+
     [TestFixture]
-    public class TestConversionArabianToRoman
+    public class TestConversionRomanToArabian
     {
-        ArabianToRomanNumberConverter _converter;
+        RomanToArabianNumberConverter _converter;
 
         [SetUp]
         public void Init()
         {
-            _converter = new ArabianToRomanNumberConverter();
+            _converter = new RomanToArabianNumberConverter();
         }
 
         [Test, TestCaseSource(typeof(GivenData), "TestBasicCases")]
-        public void TestConversion(int toBeConverted, string intendedValue)
+        public void TestConversion(int intendedValue, string toBeConverted)
         {
             var convertionResult = _converter.Convert(toBeConverted);
             convertionResult.ShouldBeEquivalentTo(intendedValue, $"because when i try to convert {toBeConverted} then i should get {intendedValue} as a result not {convertionResult}.");
