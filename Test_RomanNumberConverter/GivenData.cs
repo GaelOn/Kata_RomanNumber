545e7d44ee639ba0c7cf0dbba12abede7029038c
MoonRabbit <dupiregael@yahoo.fr>
2017-02-12 22:32:53 +0100

    add XIIII test case, red test

--- Test_RomanNumberConverter/GivenData.cs
+++ Test_RomanNumberConverter/GivenData.cs
@@ -18,6 +18,7 @@ namespace Test_RomanNumberConverter
                 yield return new TestCaseData("CCCC", "The character C is repeated 4 times which is forbiden for RomanNumber.", "because C is repeated 4 times");
                 yield return new TestCaseData("DDDD", "The character D is repeated 4 times which is forbiden for RomanNumber.", "because D is repeated 4 times");
                 yield return new TestCaseData("MMMM", "The character M is repeated 4 times which is forbiden for RomanNumber.", "because M is repeated 4 times");
+                yield return new TestCaseData("XIIII", "The character I is repeated 4 times which is forbiden for RomanNumber.", "because I is repeated 4 times");
             }
         }
 
