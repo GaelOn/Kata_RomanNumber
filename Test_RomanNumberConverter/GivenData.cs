e926d7bd25916eef2ec8af3dc43b7d663d468058
MoonRabbit <dupiregael@yahoo.fr>
2017-02-12 23:15:17 +0100

    "E" test case, red test

--- Test_RomanNumberConverter/GivenData.cs
+++ Test_RomanNumberConverter/GivenData.cs
@@ -18,6 +18,7 @@ namespace Test_RomanNumberConverter
                 yield return new TestCaseData("CCCC", "The character C is repeated 4 times which is forbiden for RomanNumber.", "because C is repeated 4 times");
                 yield return new TestCaseData("DDDD", "The character D is repeated 4 times which is forbiden for RomanNumber.", "because D is repeated 4 times");
                 yield return new TestCaseData("XIIII", "The character I is repeated 4 times which is forbiden for RomanNumber.", "because I is repeated 4 times");
+                yield return new TestCaseData("E", "The character E is not allowed in roman number.", "because E is not a valid character in roman number");
             }
         }
 
