8a064758810f2f6310cee0d8d65d92ade8f1adf4
MoonRabbit <dupiregael@yahoo.fr>
2017-02-13 23:44:37 +0100

    IVI test case (IV can not be followed by I), red test

--- Test_RomanNumberConverter/GivenData.cs
+++ Test_RomanNumberConverter/GivenData.cs
@@ -46,6 +46,7 @@ namespace Test_RomanNumberConverter
                 yield return new TestCaseData("LVX", "The combinaison VX is not an allowed one for roman number.", "because XD is not an allowed combinaison for roman number");
                 yield return new TestCaseData("DIC", "The combinaison IC is not an allowed one for roman number.", "because XD is not an allowed combinaison for roman number");
                 yield return new TestCaseData("DVC", "The combinaison VC is not an allowed one for roman number.", "because XD is not an allowed combinaison for roman number");
+                yield return new TestCaseData("IVI", "The combinaison IV can not be followed by I for roman number.", "because IVI is not an allowed combinaison for roman number");
             }
         }
 
